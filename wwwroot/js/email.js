e6fed1817355379ef766ac1791d3f8f2e08ff8f3
DanielCoronel <danicoronel2022@hotmail.com>
2025-07-19 11:32:01 -0300

    Sección de contacto integrada con EmailJS y LeafletMap

---
+++ wwwroot/js/email.js
@@ -0,0 +1,25 @@
+﻿window.sendEmail = async function (formData) {
+    try {
+        // Inicializa EmailJS (solo una vez)
+        if (!window.emailjsInitialized) {
+            emailjs.init("E4UNOHpNXV0V8q5WJ");
+            window.emailjsInitialized = true;
+        }
+
+        //console.log("✉️ Datos recibidos para enviar:", formData);
+
+        const response = await emailjs.send("service_lo7ibu6", "template_3bx9bnf", {
+            from_name: formData.FullName,
+            from_email: formData.Email,
+            phone: formData.Phone,
+            reason: formData.Reason,
+            message: formData.Message
+        });
+
+        //console.log("✅ Correo enviado con éxito:", response);
+        alert("✅ Correo enviado correctamente.");
+    } catch (error) {
+        console.error("❌ Error al enviar correo:", error);
+        alert("❌ Ocurrió un error al enviar el correo.");
+    }
+};
