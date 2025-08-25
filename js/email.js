9c9b0cbb7fd22eab983eb39ddc2f5e44ef55083b
DanielCoronel <danicoronel2022@hotmail.com>
2025-08-25 15:10:57 -0300

    Deploy to GitHub Pages

---
+++ js/email.js
@@ -0,0 +1,34 @@
+﻿window.sendEmail = async function (formData) {
+    try {
+        // Inicializa EmailJS (solo una vez)
+        if (!window.emailjsInitialized) {
+            emailjs.init("E4UNOHpNXV0V8q5WJ");
+            window.emailjsInitialized = true;
+        }
+
+        //console.log("✉️ Datos recibidos para enviar:", formData);
+
+        const response = await emailjs.send("service_lo7ibu6", "template_3bx9bnf", {
+            from_name: formData.FullName,
+            from_email: formData.Email,
+            phone: formData.Phone,
+            reason: formData.Reason,
+            modules: formData.Modules?.join(', ') || '',
+            message: formData.Message
+        });
+
+        //console.log("✅ Correo enviado con éxito:", response);
+        alert("✅ Correo enviado correctamente.");
+    } catch (error) {
+        console.error("❌ Error al enviar correo:", error);
+        alert("❌ Ocurrió un error al enviar el correo.");
+    }
+};
+
+window.getSelectedModules = (dotNetHelper) => {
+    const select = document.querySelector('select[multiple]');
+    if (!select) return;
+
+    const selected = Array.from(select.selectedOptions).map(opt => opt.value);
+    dotNetHelper.invokeMethodAsync("SetSelectedModules", selected);
+};
